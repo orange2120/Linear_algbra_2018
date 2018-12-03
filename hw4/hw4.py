8c165dd94403951bac7e03e718a2b331aaef9cbd
orange2120 <orange21201@gmail.com>
2018-12-04 02:46:23 +0800

    1204 hw4 program finished

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -19,13 +19,46 @@ def load(fname):
     return ret
 
 def get_tran(g):
-    # TODO
+    cnt = 0
+    n = g.shape[0]
+    g = np.transpose(g)
+    tm = np.zeros(shape=(n,n), dtype=float)
+
+    for i in range(n):
+        cnt = np.count_nonzero(g[i])
+        for j in range(n):
+            if g[i][j] == 1.0:
+                tm[j][i] = 1/cnt
+        cnt = 0 # reset counter
+    return tm
 
 def cal_rank(t, d = 0.85, max_iterations = 1000, alpha = 0.001):
-    # TODO
+    cnt = 0
+    n = t.shape[0]
+    r_zero = np.zeros(shape=(n,1), dtype=float)
+    r = np.ndarray(shape=(n,1), dtype=float)
+    rn = np.zeros(shape=(n,1), dtype=float)
+
+    for i in range(n):
+            r_zero[i] = 1/n # fill the zero column
+    
+    #print(r_zero)
+    #print((1-d)*r_zero)
+
+    while cnt < max_iterations:
+        rn = (1-d)*r_zero + d*(np.matmul(t, r))
+        if dist(rn, t) <= alpha:
+            print(cnt)
+            break
+        r = rn
+        cnt = cnt + 1
+    #print(cnt)
+    return rn
 
 def save(t, r):
-    # TODO
+    np.savetxt('1.txt', t)
+    np.savetxt('2.txt', r)
+    return
 
 def dist(a, b):
     return np.sum(np.abs(a-b))
